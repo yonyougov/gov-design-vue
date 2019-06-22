f2413252dfd8b53f968145406a394f3e6faf87a8
undefined <undefined>
2019-06-22 12:21:35 +0800

    ci: correct repo addr

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -21,7 +21,7 @@ cd _site
 git init
 git add -f .
 git commit -m "Travis build"
-git push --force --quiet "https://${GITHUB_TOKEN}@github.com/vueComponent/ant-design-vue.git" master:gh-pages > /dev/null
+git push --force --quiet "https://${GITHUB_TOKEN}@github.com/yonyougov/gov-design-vue.git" master:gh-pages > /dev/null
 
 
 echo "Done updating gh-pages\n"
